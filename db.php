1f347f3c4d2230d817ee28673036d8db25d21ce6
Harshit Bhatt <harshitbhatt14@gmail.com>
2016-07-02 21:51:41 -0400

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-$myserver="204.93.216.11";
-$dbuser="panky131_root";
-$dbpass="root@12345";
-$dbdb="panky131_site";
+$myserver="";
+$dbuser="";
+$dbpass="";
+$dbdb="";
 $con = mysql_connect($myserver,$dbuser,$dbpass) or die('Mysql Connection Failed');
 mysql_select_db($dbdb,$con);
 ?>
